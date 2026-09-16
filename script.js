a2ccbc69f29cdf314ad05c23ee184830c7712135
Margarita Rincon <149219844+Mago-RM@users.noreply.github.com>
2026-09-16 14:12:54 -0700

    Project Grid
    Project Grid

--- script.js
+++ script.js
@@ -7,13 +7,16 @@ const codeExperience = document.getElementById("code");
 const sjsuExperience = document.getElementById("sjsu");
 
 gamesExperience.style.display = "block";
+gamesExperience.classList.add("slide");
 codeExperience.style.display = "none";
 sjsuExperience.style.display = "none"
+gamesButton.classList.add("active");
 
 
 /*      Listeners       */
 gamesButton.addEventListener("click", function () {
     gamesExperience.style.display = "block";
+    gamesExperience.classList.add("slide");
     codeExperience.style.display = "none";
     sjsuExperience.style.display = "none";
 
@@ -25,6 +28,7 @@ gamesButton.addEventListener("click", function () {
 codeButton.addEventListener("click", function () {
     gamesExperience.style.display = "none";
     codeExperience.style.display = "block";
+    codeExperience.classList.add("slide");
     sjsuExperience.style.display = "none";
 
     gamesButton.classList.remove("active");
@@ -36,8 +40,40 @@ sjsuButton.addEventListener("click", function () {
     gamesExperience.style.display = "none";
     codeExperience.style.display = "none";
     sjsuExperience.style.display = "block";
+    sjsuExperience.classList.add("slide");
 
     gamesButton.classList.remove("active");
     codeButton.classList.remove("active");
     sjsuButton.classList.add("active");
+});
+
+
+/*              Carousel.  */
+
+const nextButton = document.querySelector(".next-project");
+const prevButton = document.querySelector(".prev-project");
+let currentProject = 0;
+const projectCards = document.querySelectorAll(".project_card");
+const featuredProject = document.querySelector(".featured-project");
+
+
+
+nextButton.addEventListener("click", function () {
+    currentProject = currentProject + 1;
+    
+    if (currentProject == projectCards.length) {
+        currentProject = 0;
+    }
+
+    featuredProject.innerHTML = projectCards[currentProject].innerHTML;
+});
+
+prevButton.addEventListener("click", function () {
+    currentProject = currentProject - 1;
+
+    if (currentProject == projectCards.length) {
+        currentProject = 0;
+    }
+
+    featuredProject.innerHTML = projectCards[currentProject].innerHTML;
 });
